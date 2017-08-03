b4ff56edfe6ab3a15b4444e12ab96449be6151ae
xiaojuan <xiaojuan@xiaojuandeMacBook-Pro.local>
2017-08-03 16:05:36 +0800

    Test deploy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const debug = require('debug')('urlencode2json')
 module.exports = (req, res, next) => {
   if(!req.is('application/x-www-form-urlencoded') || req.method != 'POST') {
     debug('skip transform urlencode to json')
+    //
     return next()
   }
   Object.keys(req.body).forEach(key => {
